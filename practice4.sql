9074eeb83fff2048e92f4d18318cc39420650bdb
thanhtuan1612 <166817710+thanhtuan1612@users.noreply.github.com>
2024-04-24 11:37:48 +0700

    Update practice4.sql

--- practice4.sql
+++ practice4.sql
@@ -14,3 +14,21 @@ when x+y>z and x+z>y and y+z>x then 'Yes' else 'No'
 END as triangle
 from triangle
 -- ex3
+  WITH uncategorised_calls AS 
+( SELECT COUNT(case_id) AS uncategorised_calls
+  FROM callers
+  WHERE call_category = 'n/a' OR call_category IS NULL)
+  
+SELECT ROUND(100.0 * uncategorised_calls::NUMERIC / (SELECT COUNT(*)
+  FROM callers), 1) AS call_percentage
+FROM uncategorised_calls;
+-- ex4
+select name from customer
+where referee_id<>2 or referee_id is null
+-- ex5
+select survived, 
+       sum(case when pclass = 1 then 1 else 0 end) as first_class,
+       sum(case when pclass = 2 then 1 else 0 end) as second_class,
+       sum(case when pclass = 3 then 1 else 0 end) as third_class
+from titanic 
+group by survived;  
